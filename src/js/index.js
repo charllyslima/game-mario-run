a8b91fa3bc6d607391b7f5ef3193b796b50ac6b7
Charllys de Lima <charllys.lima@outlook.com>
2022-07-17 21:23:31 -0300

    wip: start screen

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import "../css/style.scss";
+import "../css/interactions.scss";
+window.addEventListener("load", function () {
+    const floorPt1 = document.querySelector("#floor-pt1");
+    const floorPt2 = document.querySelector("#floor-pt2");
+
+    // floorPt1.classList.add("floor-move-pt1");
+    // floorPt2.classList.add("floor-move-pt2");
+
+    console.log("all loaded");
+});
